d521581d92cef4633aeba8831bc7f54f953b3361
altynaiabd <altynaiabdyrakhmanova@gmail.com>
2023-02-07 21:31:18 +0600

    Добавила темплейтки index.html, menu.html и статики. Внесла изменения в urls, views, settings

--- pythonProject/aksaray/aksaray_models/views.py
+++ pythonProject/aksaray/aksaray_models/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, template_name='restaurant/index.html')
+
+def menu(request):
+    return render(request, template_name='restaurant/menu.html')
